4ede991401836c0f7b077fbe1d5b6927c7084e3f
MyroshnychenkoDanylo <myroshnychenko0203@gmail.com>
2023-03-29 19:18:46 +0300

    aded a part #1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,67 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainerREF = document.querySelector(".gallery");
+const cardsItems = createElements(galleryItems);
+
+galleryContainerREF.insertAdjacentHTML("beforeend", cardsItems);
+
+function createElements() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+galleryContainerREF.addEventListener("click", onOpenModal);
+const instance = "";
+
+function onOpenModal(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  window.addEventListener("keydown", onEscapePress);
+
+  const clickOnOpenREF = event.target;
+  const indexGalleryItems = galleryItems.findIndex(
+    (option) => option.description === clickOnOpenREF.alt
+  );
+
+  let instance = basicLightbox.create(`
+    <img class="gallery__image" src="${galleryItems[indexGalleryItems].original}"
+     alt="${galleryItems[indexGalleryItems].description}">`);
+
+  instance.show();
+
+  const closeElModal = document.querySelector(".basicLightbox");
+  closeElModal.addEventListener("click", onCloseModal);
+}
+
+function onCloseModal() {
+  window.removeEventListener("keydown", onEscapePress);
+  instance.close;
+  const closeElModal = document.querySelector(".basicLightbox");
+  closeElModal.remove();
+}
+
+function onEscapePress(event) {
+  if (event.code === "Escape") {
+    const closeREF = document.querySelector(".basicLightbox");
+    closeREF.remove();
+    window.removeEventListener("keydown", onEscapePress);
+  }
+}
