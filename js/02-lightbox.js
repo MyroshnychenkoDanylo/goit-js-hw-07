899f968d87a987a6b27d13dd11bde1463f5b46bf
MyroshnychenkoDanylo <myroshnychenko0203@gmail.com>
2023-03-30 08:51:43 +0300

    added ex#2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerREF = document.querySelector(".gallery");
+
+galleryContainerREF.insertAdjacentHTML(
+  "beforeend",
+  galleryItemsMarkup(galleryItems)
+);
+
+function galleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li>
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
